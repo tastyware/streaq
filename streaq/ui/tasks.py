05222774e1ea9668e32301ab09a50fba79a99d0c
Evgeny Prilepin <esp.home@gmail.com>
2025-07-10 16:56:21 +0100

    move get_context back to tasks module

--- streaq/ui/tasks.py
+++ streaq/ui/tasks.py
@@ -1,29 +1,111 @@
 from datetime import datetime
 from typing import Annotated, Any
 
+from async_lru import alru_cache
 from fastapi import APIRouter, Depends, Form, Request, Response, status
 from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import BaseModel
 
 from streaq import TaskStatus, Worker
-from streaq.ui.context import ContextDict, get_context
+from streaq.constants import REDIS_RESULT, REDIS_RUNNING, REDIS_TASK
 from streaq.ui.deps import get_worker, templates
 
 router = APIRouter()
 
-async def _get_context(
+
+class TaskData(BaseModel):
+    color: str
+    text_color: str
+    enqueue_time: str
+    task_id: str
+    status: TaskStatus
+    fn_name: str
+    sort_time: datetime
+    url: str
+
+
+@alru_cache(ttl=1)
+async def _get_context(worker: Worker[Any], task_url: str) -> dict[str, Any]:
+    pipe = await worker.redis.pipeline(transaction=False)
+    for priority in worker.priorities:
+        await pipe.zrange(worker._queue_key + priority, 0, -1)  # type: ignore
+    await pipe.xread(
+        {worker._stream_key + p: "0-0" for p in worker.priorities},  # type: ignore
+        count=1000,
+    )
+    await pipe.keys(worker._prefix + REDIS_RESULT + "*")  # type: ignore
+    await pipe.keys(worker._prefix + REDIS_RUNNING + "*")  # type: ignore
+    await pipe.keys(worker._prefix + REDIS_TASK + "*")  # type: ignore
+    res = await pipe.execute()
+    stream: set[str] = (
+        set(t.field_values["task_id"] for v in res[-4].values() for t in v)
+        if res[-4]
+        else set()
+    )
+    queue: set[str] = set()
+    for r in res[: len(worker.priorities)]:
+        queue |= set(r)
+    results = set(r.split(":")[-1] for r in res[-3])
+    running = set(r.split(":")[-1] for r in res[-2])
+    tasks: list[TaskData] = []
+    to_fetch: list[str] = list(res[-1] | res[-3])
+    serialized = await worker.redis.mget(to_fetch) if to_fetch else ()  # type: ignore
+    for i, entry in enumerate(serialized):
+        td = worker.deserialize(entry)
+        task_id = to_fetch[i].split(":")[-1]
+        if task_id in results:
+            status = TaskStatus.DONE
+            color = "success"
+            text_color = "light"
+        elif task_id in running:
+            status = TaskStatus.RUNNING
+            color = "warning"
+            text_color = "dark"
+        elif task_id in queue:
+            status = TaskStatus.SCHEDULED
+            color = "secondary"
+            text_color = "light"
+        else:
+            status = TaskStatus.QUEUED
+            color = "info"
+            text_color = "dark"
+        ts = td.get("et") or td.get("t") or 0
+        dt = datetime.fromtimestamp(ts / 1000, tz=worker.tz)
+        tasks.append(
+            TaskData(
+                color=color,
+                text_color=text_color,
+                enqueue_time=dt.strftime("%Y-%m-%d %H:%M:%S"),
+                status=status,
+                task_id=task_id,
+                fn_name=td["f"],
+                sort_time=dt,
+                url=task_url.format(task_id=task_id),
+            )
+        )
+    tasks.sort(key=lambda td: td.sort_time)
+    return {
+        "running": len(running),
+        "queued": len(stream) - len(running),
+        "scheduled": len(queue),
+        "finished": len(results),
+        "functions": list(worker.registry.keys()),
+        "tasks": tasks,
+        "title": worker.queue_name,
+    }
+
+
+async def get_context(
     request: Request,
     worker: Annotated[Worker[Any], Depends(get_worker)],
-) -> ContextDict:
+) -> dict[str, Any]:
     task_url = request.url_for("get_task", task_id="{task_id}").path
     tasks_filter_url = request.url_for("filter_tasks").path
 
-    context = await get_context(worker, task_url)
+    context = await _get_context(worker, task_url)
     context["tasks_filter_url"] = tasks_filter_url
 
     return context
-
-
-Context = Annotated[ContextDict, Depends(_get_context)]
 
 
 @router.get("/")
@@ -33,14 +115,17 @@ async def get_root(request: Request) -> RedirectResponse:
 
 
 @router.get("/queue", response_class=HTMLResponse)
-async def get_tasks(request: Request, context: Context) -> Any:
+async def get_tasks(
+    request: Request,
+    context: Annotated[dict[str, Any], Depends(get_context)],
+) -> Any:
     return templates.TemplateResponse(request, "queue.j2", context=context)
 
 
 @router.patch("/queue", response_class=HTMLResponse)
 async def filter_tasks(
     request: Request,
-    context: Context,
+    context: Annotated[dict[str, Any], Depends(get_context)],
     functions: Annotated[list[str] | None, Form()] = None,
     statuses: Annotated[list[TaskStatus] | None, Form()] = None,
 ) -> Any:
